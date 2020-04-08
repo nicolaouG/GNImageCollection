eccd7b12189cc205a78c3d81b3d46dbd0316ee64
george <gnicolaou@axiomc.com>
2020-04-08 11:20:22 +0300

    initial project setup

---
+++ GNImageZoomCollectionViewController/GNImageZoomCollectionViewController.h
@@ -0,0 +1,19 @@
+//
+//  GNImageZoomCollectionViewController.h
+//  GNImageZoomCollectionViewController
+//
+//  Created by george on 08/04/2020.
+//  Copyright © 2020 George Nicolaou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GNImageZoomCollectionViewController.
+FOUNDATION_EXPORT double GNImageZoomCollectionViewControllerVersionNumber;
+
+//! Project version string for GNImageZoomCollectionViewController.
+FOUNDATION_EXPORT const unsigned char GNImageZoomCollectionViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GNImageZoomCollectionViewController/PublicHeader.h>
+
+
